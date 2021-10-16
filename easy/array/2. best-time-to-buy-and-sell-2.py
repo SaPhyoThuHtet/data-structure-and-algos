1d9b714189bb6712029e99007f67ae642547518a
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-10-16 18:15:48 +0630

    Create 2. best-time-to-buy-and-sell-2.py

---
+++ easy/array/2. best-time-to-buy-and-sell-2.py
@@ -0,0 +1,14 @@
+class Solution:
+  # Approach 1
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        
+        for i in range(1, len(prices)):
+             if (prices[i] > prices[i-1]):
+                    max_profit +=prices[i] - prices[i-1]
+                
+        return max_profit
+                
+        
+   # Approach 2
+        
