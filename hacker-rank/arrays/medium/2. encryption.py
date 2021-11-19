79089cc855cff2f3789f9f48074a1e63e19aa0c9
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-11-19 20:22:11 +0630

    Create 2. encryption.py

---
+++ hacker-rank/arrays/medium/2. encryption.py
@@ -0,0 +1,23 @@
+def encryption(s):
+    # Write your code here
+    s = re.sub(r' ', r'', s)
+    L = len(s)
+    row = math.floor(math.sqrt(L))
+    column = math.ceil(math.sqrt(L))
+    
+    result = ""
+    print(row, column, L)
+    
+    if(row*column <L):
+        row = column
+        
+    for i in range(column):
+            index = i
+            for j in range(row):
+                if(index < L):
+                    result += s[index]
+                    index += column
+            print(result)
+            result += " "  
+    #print(result)
+    return result 
