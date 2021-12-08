06efbc5820693db5954fbe35ea3e673019ed4413
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-12-08 22:51:53 +0630

    Create 1. POS Tagging #1.py

---
+++ hacker-rank/artificial-intelligence/nlp/1. POS Tagging #1.py
@@ -0,0 +1,2 @@
+# ဒီမှာက ဘာမှ မဟုတ်ဘူးရယ် Label လိုက်ထိုးပေးရုံပဲ
+The/DT planet/NN Jupiter/NNP and/CC its/PPS moons/NNS are/VBP in/IN effect/NN a/DT minisolar/JJ system/NN ,/, and/CC Jupiter/NNP itself/PRP is/VBZ often/RB called/VBN a/DT star/NN that/IN never/RB caught/VBN fire/NN ./.
