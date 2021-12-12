3e6d268c5b22bc1b732fd6c20a2296a16a5349b9
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-12-12 08:21:46 +0630

    Update 1. max-consecutive-ones.py

--- leetcode/1. array/1. Introduction/1. max-consecutive-ones.py
+++ leetcode/1. array/1. Introduction/1. max-consecutive-ones.py
@@ -19,3 +19,28 @@ class Solution(object):
         return max_num
 Ans: [1,1,0,1,1,1], Program's Answer => 2, Expected=> 3
 '''
+
+# First Idea ရဲ့ လိုအပ်ချက်ကို ပြင်ပြီး ခုလိုရေးလိုက်တယ်။ Time Complexity (O(n)), Space Complexity (O(1))
+"""
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        nums.append(0)
+        
+        max_num = 0
+        count_one = 0
+        
+        for i in nums:
+            if (i == 1):
+                count_one +=1
+            else:
+                if(count_one>max_num):
+                    max_num = count_one
+                count_one = 0
+                
+        return max_num
+"""
