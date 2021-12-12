c9586dbc313d4d5435fe29f678cf1e5ff68f87d6
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-12-12 08:20:06 +0630

    Update 1. max-consecutive-ones.py

--- leetcode/1. array/1. Introduction/1. max-consecutive-ones.py
+++ leetcode/1. array/1. Introduction/1. max-consecutive-ones.py
@@ -1,1 +1,21 @@
-
+#1st Idea 1 မဖြစ်ရင် Count တာကို 0 လုပ်ပြီး သွားမယ်ဆိုပြီး စဉ်းစားမိတယ်။  ဒါပေမယ့် ခု လို cases မျိုးကျ အဆင်မပြေဘူး။ End မှာက 0 မပါလို့"
+'''
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_num = 0
+        count_one = 0
+        
+        for i in nums:
+            if (i == 1):
+                count_one +=1
+            else:
+                if(count_one>max_num):
+                    max_num = count_one
+                count_one = 0
+        return max_num
+Ans: [1,1,0,1,1,1], Program's Answer => 2, Expected=> 3
+'''
