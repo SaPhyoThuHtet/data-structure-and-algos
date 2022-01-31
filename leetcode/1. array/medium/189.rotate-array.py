5cbcccb1b66a1b2da0e8d52ff899675dfd716b1e
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2022-01-31 23:15:41 +0630

    Update 189.rotate-array.py

--- leetcode/1. array/medium/189.rotate-array.py
+++ leetcode/1. array/medium/189.rotate-array.py
@@ -1,1 +1,29 @@
 # Rotate Array:  ဒီ problem ကို မြင်တာနဲ့ reverse လှန်ရမယ်ဆိုတာ သိနေတယ်။ I think it is a kind of improvement. But do not try to memorize. Please Generalize 
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        print(nums)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+        
+        return nums
+        
+    def reverse (self, nums, start, end):
+        
+        start = start
+        end = end
+        
+        while (start < end):
+            nums[start], nums[end] = nums[end], nums[start]
+            
+            start += 1
+            end -= 1
+            
+        
+        
