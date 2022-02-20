9f07ceafc4a98c623e1814d6c03b2713c22b2158
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2022-02-20 20:47:50 +0630

    Update 1. Single Number.py

--- for car/1. Array/Easy/1. Single Number.py
+++ for car/1. Array/Easy/1. Single Number.py
@@ -10,4 +10,34 @@ class Solution:
             
             
             
+# Approach 2           (Sorted စီပြီးသွားတယ်Codeကနည်းနည်းတော့ပိုရှုပ်လာတယ်)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # 1 2 2 3 3 1 
+        
+        sorted_nums = sorted(nums)
+        index = 0
+        
+         #1 1 2 3 3
+        while (index < len(sorted_nums)-1):
+            print(index)
+            if(sorted_nums[index] == sorted_nums[index+1]):
+                index += 2
+            else:
+                return sorted_nums[index]
+            
+        if (len(sorted_nums)>1): # အထူးသဖြင့် ဒီနေရာ Len( 1) နဲ့ More than Len(1) Case
+            if (sorted_nums[-1] != sorted_nums[-2]):
+                return sorted_nums[-1] 
+        
+        else: #if (len(sorted_nums) == 1):
+                return sorted_nums[0]    
+            
+        
+        
+        
+            
+            
+            
+        
         
