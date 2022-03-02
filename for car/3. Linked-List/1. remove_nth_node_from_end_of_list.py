31186da5909abf22343582c6331c7008bcd977c4
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2022-03-02 21:51:14 +0630

    Create 1. remove_nth_node_from_end_of_list.py

---
+++ for car/3. Linked-List/1. remove_nth_node_from_end_of_list.py
@@ -0,0 +1,7 @@
+Idea 1
+# ၁ ခု ဆိုရင် Node.next == None နဲ့သွားမယ်
+# ၂ ခုဆိုရင် Node.next.next = None နဲ့သွားမယ်  Etc စဥ်းစားမိတယ်။ But Code ရေးဖို့က မပြေ
+
+# Hint
+Maintain two pointers and update one with a delay of n steps.
+ဒီကောင်ကို ဘယ်လိုလုပ်ရမလဲ စဥ်းစားရ ခက်နေတယ်
