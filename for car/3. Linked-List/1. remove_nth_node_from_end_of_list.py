4db564e36f520ff9fcd9006718a3ab586da1631b
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2022-03-03 09:42:18 +0630

    Update 1. remove_nth_node_from_end_of_list.py

--- for car/3. Linked-List/1. remove_nth_node_from_end_of_list.py
+++ for car/3. Linked-List/1. remove_nth_node_from_end_of_list.py
@@ -5,6 +5,11 @@ Idea 1
 # Hint
 Maintain two pointers and update one with a delay of n steps.
 ဒီကောင်ကို ဘယ်လိုလုပ်ရမလဲ စဥ်းစားရ ခက်နေတယ် #ဒါကိုတော့ Video ကြည့်မယ်။
+Video ကိုကြည့်လိုက်တယ်။ Intuitive ဖြစ်စွာတော့ နားမလည်ဘူး။ ဒါပေမယ့် ဒီ Approach က တော်တော် အသုံးဝင်တယ်ဆိုတာ သိတယ် ဘယ်လို လုပ်လဲကြည့်ရအောင်။
+Video သာ ကြည့်လိုက်ပြီး Trace လိုက်ပေါ့။
 
 # Idea 2
-ကျိန်းသေပေါက်ရမယ့် Approach တစ်ခု ကတော့ Linked List ကို Array လို့ သဘောထားတယ်။  Elements အရေအတွက်ကို Count လုပ်မယ်။ ပြီးရင် သူ့နံပါတ်ရောက်ရင် Process လုပ် ဒါပဲ။
+ကျိန်းသေပေါက်ရမယ့် Approach တစ်ခု ကတော့ Linked List ကို Array လို့ သဘောထားတယ်။ 
+Elements အရေအတွက်ကို Count လုပ်မယ်။ ပြီးရင် သူ့နံပါတ်ရောက်ရင် Process လုပ် ဒါပဲ။
+
+
