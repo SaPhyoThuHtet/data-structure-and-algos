09563c4b0ca36718e77d148fbd90f7ba10d00bb9
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2022-03-09 11:17:57 +0630

    Update 1.group-anagram.py

--- for car/1. Array/medium/1.group-anagram.py
+++ for car/1. Array/medium/1.group-anagram.py
@@ -1,3 +1,6 @@
+ကဲ Accepted ဖြစ်သွားတာက အောင်မြင်မှု တစ်ခုပါပဲ။ Congratulations. ရသွားတာက sort လုပ် dic မှာ မရှိရင် dic ထဲထည့်။ ရှိရင် အဲ့စကားလုံးရဲ့ Index ထဲမှာ ထပ်ထည့်။
+
+
 # Time Limited Exceeded Solution
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
@@ -27,6 +30,79 @@ class Solution:
                 ans.append (ans_part)
                 
         return ans
+                
+        
+        
+# First Accepted Version
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        if strs == [""]:
+            return [[""]]
+        
+        if (len(strs) == 0 or len(strs) == 1):
+            return strs
+        
+        
+        
+        ans = []
+        
+        dic = {}
+        count = 0
+        
+        for i in range (len(strs)):
+            #print(strs[i])
+            sorted_str = ''.join(sorted(strs[i]))
+            if (sorted_str not in dic):
+                dic[sorted_str] = count
+                count +=1
+                ans.append([strs[i]])
+                #print(ans)
+                
+            else:
+                #print(dic)
+                ans[dic[sorted_str]].append(strs[i])
+                
+        return ans
+                
+        
+        
+                
+# Second Accepted Version        
+        
+        class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        '''if strs == [""]:
+            return [[""]]
+        
+        if (len(strs) == 0 or len(strs) == 1):
+            return strs'''
+        
+        
+        
+        ans = []
+        
+        dic = {}
+        count = 0
+        
+        for i in range (len(strs)):
+            #print(strs[i])
+            sorted_str = ''.join(sorted(strs[i]))
+            if (sorted_str not in dic):
+                dic[sorted_str] = count
+                count +=1
+                ans.append([strs[i]])
+                #print(ans)
+                
+            else:
+                #print(dic)
+                ans[dic[sorted_str]].append(strs[i])
+                
+        return ans
+                
+        
+        
                 
         
         
